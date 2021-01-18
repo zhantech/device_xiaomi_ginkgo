50f506e5d859fbb2f12dfe35b0ef1ba378620c95
zhantech <hifzhan41@gmail.com>
2021-01-18 17:36:13 +0000

    ginkgo: Update redfin fingerprint to Jan 2021

--- rr_ginkgo.mk
+++ rr_ginkgo.mk
@@ -48,4 +48,4 @@ RR_BUILDTYPE := Milestone
 BUILD_RR_WALLPAPERS := true
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
