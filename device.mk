190ee740f3c5789ea505e21abe16848cad4eb333
OdSazib <odsazib@gmail.com>
2021-08-09 19:22:37 -0300

    ginkgo: Build power stats and label it
    * Fixes: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    	 BatteryStatsService: Unable to load Power Hal or power.stats HAL

--- device.mk
+++ device.mk
@@ -302,7 +302,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Perf
 PRODUCT_PACKAGES += \
